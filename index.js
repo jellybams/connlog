0bd1c8657b89c70eae540be3435e80d4a321144d
spoolphiz <alexander.manea@gmail.com>
2014-11-22 14:23:02 -0800

    fix route bind

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+var express = require('express'),
+  app = express(),
+  http = require('http'),
+  bodyParser = require('body-parser'),
+  facet = require('facet-platform')();
 
-module.exports = exports = require('./lib');
+facet
+  .useModules({
+    'connlog': require('./lib/connlog')
+  })
+  .setModuleOptions({
+    dbServer: 'mongodb://connlog:loadtestwoo@ds053130.mongolab.com:53130/connlog'
+  })
+  .init(app);
+
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 9393);
+
+app.use( '/api/v1', facet.getModule('connlog').bindRoutes(express.Router()));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('connlog server listening on port ' + app.get('port'));
+});
